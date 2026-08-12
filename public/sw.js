a554ead534a513a765d2c0dae10755b04baef128
Muhammed Rafi <136684534+muhammedrafii@users.noreply.github.com>
2026-08-12 16:46:35 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'shop-ledger-v1';
+const CACHE_NAME = 'shop-ledger-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon.svg'
+  '/icon.svg',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
